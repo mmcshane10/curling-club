3da822938efbd59b45a5c81a0917d44ce0a56168
Mike <mmcshane10@gmail.com>
2019-12-11 11:29:19 -0800

    add external links to affialiates component

--- src/components/Affiliations.jsx
+++ src/components/Affiliations.jsx
@@ -11,15 +11,15 @@ function Affiliations() {
       <h2 className='heading'>Our Affiliations</h2>
       <div className='row'>
         <div className='col s4'>
-          <img className='affImage' src={mopac} />
+          <a href='http://www.mopacca.org/' target='_blank'><img className='affImage' src={mopac} /></a>
           <p className='paragraph'>MoPac is one of ten regions in the United States and represents: Arizona, California, Idaho, Montana, Nevada, Oregon and Utah. MoPac has 17 member clubs and hosts the playdowns for the United States Curling Association, USCA.</p>
         </div>
         <div className='col s4'>
-          <img className='affImage' src={usacurling} />
+          <a href='https://www.teamusa.org/usa-curling' target='_blank'><img className='affImage' src={usacurling} /></a>
           <p className='paragraph'>The United States Curling Association (USCA), doing business as USA Curling, is the National Governing Body for the Olympic sport of curling. The association is headquartered in Stevens Point, Wisconsin.</p>
         </div>
         <div className='col s4'>
-          <img className='affImage' src={wcf} />
+          <a href='https://worldcurling.org/' target='_blank'><img className='affImage' src={wcf} /></a>
           <p className='paragraph'>The World Curling Federation represents curling internationally and facilitates the growth of the sport through a network of Member Associations / Federations. It is the world governing body of the Olympic Winter Sport of Curling and the Paralympic Winter Sport of Wheelchair Curling.</p>
         </div>
       </div>
