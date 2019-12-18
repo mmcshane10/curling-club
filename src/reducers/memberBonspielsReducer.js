064391e8e2293f697ac9891e136990681273d161
Mike <mmcshane10@gmail.com>
2019-12-18 13:22:34 -0800

    add checkers to prevent duplicate registrations of leagues and bonspiels

--- src/reducers/memberBonspielsReducer.js
+++ src/reducers/memberBonspielsReducer.js
@@ -5,12 +5,16 @@ const memberBonspielsReducer = (state = initialState.memberBonspiels, action) =>
   switch (action.type) {
     case types.ADD_MEMBER_TO_BONSPIEL:
       let addMember = action.currentUser.displayName;
-      let newArray = state[action.id].members
-      return {
-        ...state,
-        [action.id]: {
-          ...state[action.id],
-          members: [...newArray, addMember]
+      let newArray = state[action.id].members;
+      if (newArray.includes(addMember)) {
+        alert('You\'ve already joined this league!');
+      } else {
+        return {
+          ...state,
+          [action.id]: {
+            ...state[action.id],
+            members: [...newArray, addMember]
+          }
         }
       }
     default:
--- src/reducers/memberLeaguesReducer.js
+++ src/reducers/memberLeaguesReducer.js
@@ -5,15 +5,20 @@ const memberLeaguesReducer = (state = initialState.memberLeagues, action) => {
   switch (action.type) {
     case types.ADD_MEMBER_TO_LEAGUE:
       let addMember = action.currentUser.displayName;
-      let newArray = state[action.id].members
-      return {
-        ...state,
-        [action.id]:{ 
-          ...state[action.id],
-          members : [...newArray, addMember] }
+      let newArray = state[action.id].members;
+      if (newArray.includes(addMember)) {
+        alert('You\'ve already joined this league!');
+      } else {
+        return {
+          ...state,
+          [action.id]: {
+            ...state[action.id],
+            members: [...newArray, addMember]
+          }
+        }
       }
     default:
-      return state 
+      return state
   }
 };
 
